b476a18681d4c7256233790510fcd35e9c3ce013
rafah <rafah.hawa@yahoo.fr>
2023-08-31 21:03:13 +0200

    add blog post_list, post_detail

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-
-
 from taggit.managers import TaggableManager
 from django.contrib.auth.models import User 
+from django.utils import timezone
 
-# Create your models here.
 '''
 title
 content
@@ -18,6 +16,11 @@ class Post(models.Model):
     author=models.ForeignKey(User,related_name='post_author', on_delete=models.SET_NULL,null=True)
     title=models.CharField(max_length=120)
     content=models.TextField( max_length=30000)
-    publish_date=models.DateTimeField()
+    publish_date=models.DateTimeField(default=timezone.now)
     tags = TaggableManager()
-    
+
+class Comment(models.Model):
+    user =models.ForeignKey(User, related_name='comment_author', on_delete=models.SET_NULL,null=True)
+    post=models.ForeignKey(User, related_name='comment_post', on_delete=models.CASCADE)
+    comment=models.TextField(max_length=30000)
+    create_at=models.DateTimeField(default=timezone.now)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post 
+from .models import Comment
 # Create your views here.
+def post_list(request):
+    data = Post.objects.all()
+    return render(request,'all_post.html',{'posts':data})
+
+def post_detail(request,post_id):
+    data = Post.objects.get(id=post_id)
+    return render(request,'post.html',{'post':data})
