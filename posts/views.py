a391908e90f855c476ad9ed91da560fc151718f8
rafah <rafah.hawa@yahoo.fr>
2023-09-07 12:12:06 +0200

    add new post

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post 
 from .models import Comment
+from .forms import PostForm
 # Create your views here.
 def post_list(request):
     data = Post.objects.all()
@@ -9,3 +10,14 @@ def post_list(request):
 def post_detail(request,post_id):
     data = Post.objects.get(id=post_id)
     return render(request,'post.html',{'post':data})
+
+def add_post(request):
+    if request.method =="POST":
+        form=PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    else:
+        form = PostForm()
+            
+    return render(request,'add_post.html',{'form':form})
--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from posts.views import post_list
 from posts.views import post_detail
+from posts.views import add_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('blog/',post_list),
+    path('blog/add',add_post),
+    
     path('blog/<int:post_id>', post_detail),
 ]
